136f7c9321e22b35bba2a3888e89d12ddeb42caf
Sumi Ryu <31054795+sumicanvas@users.noreply.github.com>
2026-07-10 17:42:31 +0900

    Delete src_scenario directory

--- src_scenario/scenario02_news_source.js
+++
@@ -1,9 +0,0 @@
-import { equalsFilter, noKeywordPipeline, runScenario } from "./common.js";
-
-await runScenario({
-  scenario: "news_mig 시나리오 2",
-  description: "뉴스구분 조건만 사용해 해당 뉴스매체의 전체 뉴스를 최신순으로 조회합니다.",
-  inputs: [{ key: "dgubun", label: "뉴스구분", example: "P" }],
-  buildPipeline: ({ indexName, limit, dgubun }) =>
-    noKeywordPipeline({ indexName, limit, filters: [equalsFilter("dgubun", dgubun)] }),
-});
--- src_scenario/scenario07_keyword.js
+++
@@ -1,8 +0,0 @@
-import { keywordPipeline, runScenario } from "./common.js";
-
-await runScenario({
-  scenario: "news_mig 시나리오 7",
-  description: "검색어만 사용해 전체 뉴스 title을 검색합니다.",
-  inputs: [{ key: "query", label: "검색어", example: "삼성전자" }],
-  buildPipeline: ({ indexName, limit, query }) => keywordPipeline({ indexName, limit, query }),
-});
